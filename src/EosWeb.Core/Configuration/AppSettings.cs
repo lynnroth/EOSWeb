1f8503ac5923aa6beb45ecec241f9b29d356c956
Lynn A. Roth <lynnr@penguin.nu>
2019-12-23 19:43:46 -0500

    Configuration file
    #1

---
+++ src/EosWeb.Core/Configuration/AppSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EosWeb.Core.Configuration
+{
+    public class AppSettings
+    {
+        public string EosOSCHost { get; set; }
+        public int EosOSCPort { get; set; }
+    }
+}
