3d8e47ba4c896eb1170802e4e03dd78290d248cc
Lynn A. Roth <lynnr@penguin.nu>
2019-12-28 22:00:18 -0500

    Add extension method for parsing Int

---
+++ src/EosWeb.Core/Tools/Extensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace System
+{
+    public static class Extensions
+    {
+        public static int ToInt32(this object value)
+        {
+            if (int.TryParse(value.ToString(), out int i))
+            {
+                return i;
+            }
+
+            throw new InvalidOperationException("Unable to parse as int");
+        }
+    }
+}
