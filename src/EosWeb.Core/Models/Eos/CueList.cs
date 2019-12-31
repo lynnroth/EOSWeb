4027e54d6d6daad0b86172f4828ac996ccdb0fd7
Lynn A. Roth <lynnr@penguin.nu>
2019-12-31 17:16:27 -0500

    Move cue to its own file

--- src/EosWeb.Core/Models/Eos/CueList.cs
+++ src/EosWeb.Core/Models/Eos/CueList.cs
@@ -25,35 +25,4 @@ namespace EosWeb.Core.Models.Eos
             return sb.ToString();
         }
     }
-
-    public class Cue
-    {
-        public bool Active { get; set; }
-        
-        public string RowClass
-        {
-            get
-            {
-                return Active ? "cue-active border-light" : "";
-            }
-        }
-        public int Index { get; set; }
-        public int Number { get; set; }
-        public string NumberWithPart
-        {
-            get { return Number + (PartNumber == 0 ? "" : "." + PartNumber); }
-        }
-        public int PartNumber { get; set; }
-        public string UID { get; set; }
-        public string Label { get; set; }
-        public TimeSpan UpIntensityDuration { get; set; }
-        public TimeSpan DownIntensityDuration { get; set; }
-        public string Note { get; set; }
-
-        public override string ToString()
-        {
-            return $"Index: {Index}  Number: {Number}  Label: {Label}  Up: {UpIntensityDuration}  Down: {DownIntensityDuration}";
-        }
-
-    }
 }
